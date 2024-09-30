61370327ae6400e8fb40d2743fb8a12ca5ee1a6d
BAbdoul <pad1934@hotmail.fr>
2024-09-30 19:00:33 +0200

    separation des modules étape 10 - Terminer

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -1,6 +1,51 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+const Header = ({ course }) => {
+    return (
+        <div>
+            <h2>{course.name}</h2>
+        </div>
+    );
+};
+
+const Part = ({ part, exercises }) => {
+    return (
+        <div>
+            <p>
+                {part} {exercises}
+            </p>
+        </div>
+    );
+};
+
+const Content = ({ course }) => {
+    return (
+        <div>
+            {course.parts.map((part) => {
+                return (
+                    <Part
+                        key={part.id}
+                        part={part.name}
+                        exercises={part.exercises}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+const Total = ({ course }) => {
+    const initialValue = 0;
+
+    const total = course.parts.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.exercises,
+        initialValue
+    );
+
+    return (
+        <>
+            <h3>Total of {total} exercises</h3>
+        </>
+    );
+};
 
 const Course = ({ course }) => {
     return (
--- courseinfo/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = ({ course }) => {
-    return (
-        <div>
-            <h2>{course.name}</h2>
-        </div>
-    );
-};
-
-export default Header;
