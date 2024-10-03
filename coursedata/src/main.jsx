acd80a64c7d5965bda126f99121576518c31fa37
BAbdoul <pad1934@hotmail.fr>
2024-10-03 09:46:46 +0200

    c/ Obtenir des données du serveur - Hooks d'effet

--- coursedata/src/main.jsx
+++ coursedata/src/main.jsx
@@ -1,30 +1,11 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import axios from "axios";
 import App from "./App.jsx";
 
-const promise = axios.get("http://localhost:3001/notes");
-console.log(promise);
-
-promise.then((response) => {
-    console.log("response", response);
-});
-
-axios.get("http://localhost:3001/notes").then((response) => {
-    const notes = response.data;
-    console.log("notes", notes);
-});
-
-const promise2 = axios.get("http://localhost:3001/foobar");
-console.log(promise2);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-axios.get("http://localhost:3001/notes").then((response) => {
-    const notes = response.data;
-    root.render(
-        <StrictMode>
-            <App notes={notes} />
-        </StrictMode>
-    );
-});
+root.render(<App />);
+// root.render(
+//     <StrictMode>
+//         <App />
+//     </StrictMode>
+// );
