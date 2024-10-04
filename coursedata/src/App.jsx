d87750ebc4848525b06567c52f31fbe0c30edbe3
BAbdoul <pad1934@hotmail.fr>
2024-10-05 01:33:58 +0200

    d/ Modification des données côté serveur - Envoi de données au serveur

--- coursedata/src/App.jsx
+++ coursedata/src/App.jsx
@@ -8,32 +8,28 @@ const App = () => {
     const [showAll, setShowAll] = useState(true);
 
     useEffect(() => {
-        console.log("effect");
         axios.get("http://localhost:3001/notes").then((response) => {
-            console.log("promise fulfilled");
             setNotes(response.data);
         });
     }, []);
 
-    console.log("render", notes.length, "notes");
-
     const addNote = (event) => {
         event.preventDefault();
-        console.log("button clicked", event.target);
-
         const noteObject = {
             content: newNote,
-            date: new Date().toISOString(),
+            date: new Date(),
             important: Math.random() < 0.5,
-            id: notes.length + 1,
         };
 
-        setNotes(notes.concat(noteObject));
-        setNewNote("");
+        axios
+            .post("http://localhost:3001/notes", noteObject)
+            .then((response) => {
+                setNotes(notes.concat(response.data));
+                setNewNote("");
+            });
     };
 
     const handleNoteChange = (event) => {
-        console.log(event.target.value);
         setNewNote(event.target.value);
     };
 
